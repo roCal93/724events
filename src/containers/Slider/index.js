72a181a6b81673a2d5137258d76f2cd37509b4b2
Romain Calmelet <romaincalmelet@gmail.com>
2025-04-09 14:29:23 +0200

    sliderOk

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,50 +8,53 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+
+  useEffect(() => {
+
+    const timer = setTimeout(
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
-  };
-  useEffect(() => {
-    nextCard();
-  });
+
+    // Nettoyer le timer
+    return () => clearTimeout(timer);
+  }, [index, byDateDesc?.length]);
+
   return (
     <div className="SlideCardList">
+      {/* Les slides */}
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
+        >
+          <img src={event.cover} alt={event.title} />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+
+      {/* La pagination */}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event) => (
+            <input
+              key={event.title}
+              type="radio"
+              name="radio-button"
+              checked={index === byDateDesc.indexOf(event)}
+              onChange={() => setIndex(byDateDesc.indexOf(event))}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -16,6 +16,7 @@ export const api = {
   },
 };
 
+
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
@@ -30,7 +31,8 @@ export const DataProvider = ({ children }) => {
     if (data) return;
     getData();
   });
-  
+
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
